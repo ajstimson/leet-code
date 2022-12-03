d570994e4429383c008f92e0e87fe1b6d0138a31
Andrew Stimson <andrew@applejuice.codes>
2022-12-02 20:31:28 -0800

    Two Sum

---
+++ December/leet-code-120222.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i]
+        let second = nums.indexOf(diff, i + 1)
+
+        if (second > 0) {
+            return [i, second]
+        }
+    }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 2, 4], 6))
+console.log(twoSum([3, 3], 6))
