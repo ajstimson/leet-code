6775c594f9a682c696e58d48603c585f1b819801
Andrew Stimson <andrew@applejuice.codes>
2022-12-03 23:26:48 -0800

    9. Palindrome Number

---
+++ December/leet-code-120322.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    return x === +Array.from(String(x), Number).reverse().join("")
+}
+
+console.log(isPalindrome(121))
+console.log(isPalindrome(-121))
