4e0547490e3698174730dd01c1e8f6f72115faea
Brux Feeblewitz <brux@pop-os.localdomain>
2023-01-07 20:57:19 -0800

    Longest Palindrome (TS) --init

---
+++ January/leet-code-010723.js
@@ -0,0 +1,6 @@
+function longestPalindrome(s) {
+    console.log("bad");
+    console.log(Array.from(String(s)).reverse().join(""));
+    return "";
+}
+longestPalindrome("babad");
---
+++ January/leet-code-010723.ts
@@ -0,0 +1,6 @@
+function longestPalindrome(s: string): string {
+    console.log(Array.from(String(s)).reverse().join(""))
+    return ""
+}
+
+longestPalindrome("babad")
