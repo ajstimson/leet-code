35d5e375c158c1605fb0d877d075c11616a1df71
Brux Feeblewitz <ajstimson@gmail.com>
2023-01-07 21:03:44 -0800

    Longest Palindrome (TS) First Attempt

--- January/leet-code-010723.ts
+++ January/leet-code-010723.ts
@@ -1,6 +1,19 @@
 function longestPalindrome(s: string): string {
-    console.log(Array.from(String(s)).reverse().join(""))
+    const isPalindrome = (s: string) => {
+        return Array.from(String(s)).reverse().join("") === s
+    }
+    for (let i = s.length; i > 0; i--) {
+        for (let j = 0; j < s.length; j++) {
+            if (j + i > s.length) {
+                break
+            }
+            const sub = s.substr(j, i)
+            if (isPalindrome(sub)) {
+                return sub
+            }
+        }
+    }
     return ""
 }
 
-longestPalindrome("babad")
+console.log(longestPalindrome("babad"))
