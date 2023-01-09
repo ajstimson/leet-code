84f012e49f4fc0c0c9c2599e583d804119bc705d
Brux Feeblewitz <ajstimson@gmail.com>
2023-01-08 23:14:59 -0800

    Longest Palindrome (TS) Final Solution

--- January/leet-code-010723.ts
+++ January/leet-code-010723.ts
@@ -1,23 +1,36 @@
-function longestPalindrome(s: string) {
+/*  
+The function longestPalindrome returns the longest palindromic substring in the 
+input string s. It does this by iterating through each character in s and finding 
+the longest palindromic substring centered at that character. The length of this 
+substring is determined by calling the helper function searchFromCenter, which 
+expands the substring outwards from the center until it is no longer a palindrome. 
+It then returns the length of this substring.
+
+longestPalindrome keeps track of the maximum length palindromic substring it has 
+seen so far and the start index of that substring. It updates these values if it 
+finds a longer palindromic substring. At the end, it returns the substring with the 
+maximum length by using the start index and length stored in maxSubStart and 
+maxSubLength.
+*/
+
+function longestPalindrome(s: string): string {
     if (s.length < 1) return ""
-    let maxSubStart = 0
-    let maxSubLength = 0
+    let start = 0
+    let strLength = 0
     for (let i = 0; i < s.length; i++) {
-        const lengthCenteredAtChar = expandAroundCenter(s, i, i)
-        const lengthCenteredAtSpace = expandAroundCenter(s, i, i + 1)
-        const longestSubAtChar = Math.max(
-            lengthCenteredAtChar,
-            lengthCenteredAtSpace
+        const longest = Math.max(
+            searchFromCenter(s, i, i),
+            searchFromCenter(s, i, i + 1)
         )
-        if (longestSubAtChar > maxSubLength) {
-            maxSubLength = longestSubAtChar
-            maxSubStart = i - Math.floor((maxSubLength - 1) / 2)
+        if (longest > strLength) {
+            strLength = longest
+            start = i - Math.floor((strLength - 1) / 2)
         }
     }
-    return s.substr(maxSubStart, maxSubLength)
+    return s.slice(start, start + strLength)
 }
 
-function expandAroundCenter(s: string, left: number, right: number) {
+function searchFromCenter(s: string, left: number, right: number): number {
     while (left >= 0 && right < s.length && s[left] === s[right]) {
         left--
         right++
